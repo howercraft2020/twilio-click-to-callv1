be16ef0f6524067c49cf266ac6a5f6a52abc9867
diegoubicua <diego.maldonado@ubicua.cl>
2020-06-11 21:12:38 -0400

    Initial commit

---
+++ src/main/java/cl/ubicua/twilioclicktocall/app/TwilioClickToCallSpringBootV1Application.java
@@ -0,0 +1,13 @@
+package cl.ubicua.twilioclicktocall.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioClickToCallSpringBootV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwilioClickToCallSpringBootV1Application.class, args);
+    }
+
+}
